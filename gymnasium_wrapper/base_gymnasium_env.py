f89ec71d86d5159af2d87a7c95ce85d2d75ad921
Marek Wydmuch <marek@wydmuch.poznan.pl>
2026-02-08 00:36:14 +0100

    Update copyrights and licence notes

--- gymnasium_wrapper/base_gymnasium_env.py
+++ gymnasium_wrapper/base_gymnasium_env.py
@@ -2,10 +2,34 @@
 Implementation of the base Gymnasium environment for ViZDoom.
 
 The first version was based on Gym interface by [Simon Hakenes](https://github.com/shakenes/vizdoomgym),
-and developed by [Arjun KG](https://github.com/arjun-kg),
+and developed by
+[Arjun KG](https://github.com/arjun-kg),
 [Benjamin Noah Beal](https://github.com/bebeal),
 [Lawrence Francis](https://github.com/ldfrancis),
-and [Mark Towers](https://github.com/pseudo-rnd-thoughts).
+[Mark Towers](https://github.com/pseudo-rnd-thoughts).
+
+Modified and updated by
+[Muhammad Elnimr](https://github.com/melnimr),
+[Hugo Huang](https://github.com/Trenza1ore),
+[Marek Wydmuch](https://github.com/mwydmuch).
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 """
 
 import itertools
