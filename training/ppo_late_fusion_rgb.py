fe521d0768ec62a570c8dc9c685ade1c9fc5447f
DrewLoader <loaderandrew1@gmail.com>
2026-02-17 15:20:35 -0800

    training 10 epochs

--- training/ppo_late_fusion_rgb.py
+++ training/ppo_late_fusion_rgb.py
@@ -46,8 +46,8 @@ episodes_to_watch = 10
 # Persistence
 model_savefile = "../models/defend_the_line/ppo_late_fusion_rgb.pth"
 save_model     = True
-load_model     = True
-skip_learning  = True
+load_model     = False
+skip_learning  = Falase
 
 config_file_path = os.path.join(SCENARIO_PATH, "defend_the_line.cfg")
 print(config_file_path)
