fe2b45885087a2a1603985b0f73069671f5acfb0
Sean Nguyen <smnguyen745@gmail.com>
2026-02-25 13:32:59 -0800

    fix: update default q_rainbow_stacked scenario

--- training/q_rainbow_stacked.py
+++ training/q_rainbow_stacked.py
@@ -45,8 +45,8 @@ from utils import *  # shared constants + preprocess functions
 # -----------------------------------------------------------------------------
 # Scenario / save naming
 # -----------------------------------------------------------------------------
-# SCENARIO_NAME = "defend_the_center"
-SCENARIO_NAME = "deadly_corridor"
+SCENARIO_NAME = "defend_the_center"
+# SCENARIO_NAME = "deadly_corridor"
 config_file_path = os.path.join(SCENARIO_PATH, f"{SCENARIO_NAME}.cfg")
 
 # MODEL_TYPE = os.path.splitext(os.path.basename(__file__))[0] + "2"
